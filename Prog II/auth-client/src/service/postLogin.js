5e1cb0934aba3bfd7ef38837e47bd965933fd297
Jonathan Gotz <jonathansgore@hotmail.com>
2024-07-08 23:14:46 -0300

    mensagem erro login consertado

--- Prog II/auth-client/src/service/postLogin.js
+++ Prog II/auth-client/src/service/postLogin.js
@@ -1,15 +1,17 @@
 async function postLogin(data){
-    const response = await fetch('http://localhost:8080/usuario/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    });
+    
+        const response = await fetch('http://localhost:8080/usuario/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
     if (!response.ok) {
         const error = await response.json();
-        throw new Error(error.errors[0]);
+        throw new Error(error.message);
     }
+    
     return await response.json();
 }
 
