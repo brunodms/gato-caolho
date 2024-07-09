ff576b372dfcd18b36eb735105c2026b8ef3617f
Jonathan Gotz <jonathansgore@hotmail.com>
2024-07-08 22:33:41 -0300

    models removidos e funções de create e delete add

--- Prog II/auth-system/src/models/Comanda.js
+++
@@ -1,9 +0,0 @@
-class Comanda {
-    constructor(id_comanda){
-        this.id_comanda = id_comanda;
-    }
-
-        getId_comanda = function(){
-            return this.id_comanda;
-        }
-    }
