9ee395a04736cf02ea4d029f199bd31e2f18d60e
Bruno de Macedo Sanagiotto <brunomacedosana@gmail.com>
2024-07-08 23:24:33 -0300

    criado pagina de registro de usuarios

---
+++ Prog II/auth-client/src/components/Register.jsx
@@ -0,0 +1,159 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+import { Button, createTheme, TextField, Box, Stack } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+import PropTypes from 'prop-types';
+
+import postRegister from "../service/postRegister";
+import Header from "./Header";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    cpf: "",
+    nome: "",
+    email: "",
+    senha: "",
+    data_admissao: "",
+    id_cargo: "",
+    telefone: "",
+  });
+
+  const handleRegisterSuccess = (response) => {
+    console.log("Registro bem-sucedido", response);
+    // Navigate to the login page or perform any other action
+  };
+
+  const basics = createTheme({
+    // Same theme configuration as in the Login component
+  });
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {
+      cpf: formData.cpf,
+      nome: formData.nome,
+      email: formData.email,
+      senha: formData.senha,
+      data_admissao: formData.data_admissao,
+      id_cargo: formData.id_cargo,
+      telefone: formData.telefone,
+    };
+
+    try {
+      const response = await postRegister(data);
+      handleRegisterSuccess(response);
+    } catch (error) {
+      console.log("erro ao registrar", error);
+    }
+  };
+
+  return (
+    <ThemeProvider theme={basics}>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "60%" },
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 20,
+          marginBottom: 2
+        }}
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit}
+      >
+        <Header title="Registrar gato caolho" />
+        <TextField
+          id='register_cpf'
+          label="CPF"
+          variant="outlined"
+          name="cpf"
+          placeholder="cpf"
+          type="text"
+          value={formData.cpf}
+          onChange={handleChange}
+        />
+        <TextField
+          id='register_nome'
+          label="Nome"
+          variant="outlined"
+          name="nome"
+          placeholder="nome"
+          type="text"
+          value={formData.nome}
+          onChange={handleChange}
+        />
+        <TextField
+          id='register_email'
+          label="Email"
+          variant="outlined"
+          name="email"
+          placeholder="email"
+          type="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <TextField
+          id='register_senha'
+          label="Senha"
+          variant="outlined"
+          name="senha"
+          placeholder="senha"
+          type="password"
+          value={formData.senha}
+          onChange={handleChange}
+        />
+        <TextField
+          id='register_data_admissao'
+          label="Data de Admissão"
+          variant="outlined"
+          name="data_admissao"
+          placeholder="data de admissão"
+          type="date"
+          value={formData.data_admissao}
+          onChange={handleChange}
+        />
+        <TextField
+          id='register_id_cargo'
+          label="ID do Cargo"
+          variant="outlined"
+          name="id_cargo"
+          placeholder="id do cargo"
+          type="number"
+          value={formData.id_cargo}
+          onChange={handleChange}
+        />
+        <TextField
+          id='register_telefone'
+          label="Telefone"
+          variant="outlined"
+          name="telefone"
+          placeholder="telefone"
+          type="text"
+          value={formData.telefone}
+          onChange={handleChange}
+        />
+        <Stack spacing={2} direction="row" sx={{
+          "& > :not(style)": { m: 1, width: "60%" },
+          justifyContent: "center"
+        }}>
+          <Button id='registrar' className="Button" variant="outlined" type="submit">
+            Registrar
+          </Button>
+        </Stack>
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+Register.propTypes = {
+  onRegisterSuccess: PropTypes.func.isRequired,
+};
+
+export default Register;
