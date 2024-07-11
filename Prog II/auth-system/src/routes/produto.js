46673a6a9650b8cb5100a7cc5f1ea7db39929373
Jonathan Gotz <90211395+Jonagotz@users.noreply.github.com>
2024-07-11 03:13:46 -0300

    middleware requisitando login adicionado

--- Prog II/auth-system/src/routes/produto.js
+++ Prog II/auth-system/src/routes/produto.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import produtoController from '../controllers/Produto';
+import loginRequired from '../middlewares/loginRequired';
 
 const routes = new Router();
 
-routes.get('/', produtoController.index);
-routes.post('/create', produtoController.create_produto);
-routes.put('/update/:id', produtoController.update_produto);
-routes.delete('/delete/:id', produtoController.delete_produto);
-routes.get('/:id', produtoController.get_produto_by_id);
+routes.get('/', loginRequired, produtoController.index);
+routes.post('/create', loginRequired, produtoController.create_produto);
+routes.put('/update/:id', loginRequired, produtoController.update_produto);
+routes.delete('/delete/:id', loginRequired, produtoController.delete_produto);
+routes.get('/:id', loginRequired, produtoController.get_produto_by_id);
 
 export default routes;
--- Prog II/auth-system/src/routes/usuario.js
+++ Prog II/auth-system/src/routes/usuario.js
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import usuarioController from '../controllers/Usuario';
+import loginRequired from '../middlewares/loginRequired';
 
 const routes = new Router();
 
 // Rota para a página inicial da API
-routes.get('/', usuarioController.index);
-routes.get('/:id', usuarioController.getById);
-routes.put('/update_usuario/:id', usuarioController.update_usuario);
+routes.get('/', loginRequired, usuarioController.index);
+routes.get('/:id', loginRequired, usuarioController.getById);
+routes.put('/update_usuario/:id', loginRequired, usuarioController.update_usuario);
 routes.post('/login/', usuarioController.login);
-routes.post('/register/', usuarioController.createUsuario);
-routes.delete('/delete_usuario/:id', usuarioController.delete_usuario);
+routes.post('/register/', loginRequired, usuarioController.createUsuario);
+routes.delete('/delete_usuario/:id', loginRequired, usuarioController.delete_usuario);
 
 export default routes;
